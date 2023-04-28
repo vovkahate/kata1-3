e28c7f512dc48406c16eec66e924268d05561688
vovka <lesnikov.vladimir@gmail.com>
2023-04-29 00:04:47 +0300

    доделал слайдер 1.5

--- swip.js
+++ swip.js
@@ -1,9 +1,17 @@
 var init = false;
-let pokaz = document.querySelector('.exp');
-
+let show = document.querySelector('.exp');
+let eighth = document.querySelector('.swiper-slide:nth-of-type(8)');
+let seventh = document.querySelector('.swiper-slide:nth-of-type(7)');
+let ninth = document.querySelector('.swiper-slide:nth-of-type(9)');
+let tenth = document.querySelector('.swiper-slide:nth-of-type(10)');
 
 function swiperCard() {
     if (window.innerWidth < 768) {
+        eighth.classList.remove('hidden');
+        seventh.classList.remove('hidden');
+        ninth.classList.remove('hidden');
+        tenth.classList.remove('hidden');
+
         if (!init) {
             init = true;
             swiper = new Swiper(".swiper", {
@@ -18,33 +26,60 @@ function swiperCard() {
                 },
             });
         }
-        pokaz.classList.add('hide');
+        show.classList.add('hide');
 
-    } else if (init) {
+        } else if (init) {
         swiper.destroy();
         init = false;
-        pokaz.classList.remove('hide');
+        show.classList.remove('hide');
     }
-}
 
+
+    else if (window.innerWidth >768 && window.innerWidth <= 1120) {
+            eighth.classList.add('hidden');
+            seventh.classList.add('hidden');
+            ninth.classList.add('hidden');
+            tenth.classList.add('hidden');
+            show.innerHTML = 'Показать всё';
+        show.classList.remove('collapse');
+        show.classList.add('exp');
+        }
+    else if (window.innerWidth > 1120) {
+        ninth.classList.add('hidden');
+        tenth.classList.add('hidden');
+        eighth.classList.remove('hidden');
+        seventh.classList.remove('hidden');
+        show.innerHTML = 'Показать всё';
+        show.classList.remove('collapse');
+        show.classList.add('exp');
+    }
+
+
+    }
 
 
 swiperCard();
 window.addEventListener("resize", swiperCard);
 
 
-let eighth = document.querySelector('.swiper-slide:nth-of-type(8)');
-let seventh = document.querySelector('.swiper-slide:nth-of-type(7)');
-
-
-
-
 function myFunction() {
-    pokaz.innerHTML =
-        (pokaz.innerHTML === 'Показать всё') ? pokaz.innerHTML = 'Скрыть' : pokaz.innerHTML = 'Показать всё';
-    pokaz.classList.toggle('collapse');
-    pokaz.classList.toggle('exp');
-    eighth.classList.toggle('hidden');
-    seventh.classList.toggle('hidden');
+    show.innerHTML =
+        (show.innerHTML === 'Показать всё') ? show.innerHTML = 'Скрыть' : show.innerHTML = 'Показать всё';
+        show.classList.toggle('collapse');
+        show.classList.toggle('exp');
+
+    if (window.innerWidth > 1120) {
+        ninth.classList.toggle('hidden');
+        tenth.classList.toggle('hidden');
+
+    }
+    else if (window.innerWidth >= 768 && window.innerWidth <= 1120) {
+        eighth.classList.toggle('hidden');
+        seventh.classList.toggle('hidden');
+        ninth.classList.toggle('hidden');
+        tenth.classList.toggle('hidden');
+
+    }
 }
+
 
